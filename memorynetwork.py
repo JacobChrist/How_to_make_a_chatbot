da1b5f23a90a10e1f8eb1d9b3e16f145768c5763
Jacob Christ <jacob@pontech.com>
2017-04-04 15:46:33 -0700

    I'm able to predict an output based on test data!

--- memorynetwork.py
+++ memorynetwork.py
@@ -11,7 +11,7 @@ Time per epoch: 3s on CPU (core i7).
 '''
 from __future__ import print_function
 
-from keras.models import Sequential, Model
+from keras.models import Sequential, Model, model_from_json
 from keras.layers.embeddings import Embedding
 from keras.layers import Input, Activation, Dense, Permute, Dropout, add, dot, concatenate
 from keras.layers import LSTM
@@ -21,6 +21,7 @@ from functools import reduce
 import tarfile
 import numpy as np
 import re
+import json
 
 
 def tokenize(sent):
@@ -118,7 +119,7 @@ vocab = set()
 for story, q, answer in train_stories + test_stories:
     vocab |= set(story + q + [answer])
 vocab = sorted(vocab)
-
+    
 # Reserve 0 for masking via pad_sequences
 vocab_size = len(vocab) + 1
 story_maxlen = max(map(len, (x for x, _, _ in train_stories + test_stories)))
@@ -133,14 +134,23 @@ print('Number of test stories:', len(test_stories))
 print('-')
 print('Here\'s what a "story" tuple looks like (input, query, answer):')
 print(train_stories[0])
+#for t in range(0, len(train_stories)):
+#  print(t, train_stories[t])
 print('-')
 print('Vectorizing the word sequences...')
 
 word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
+idx_word = dict((i + 1, c) for i, c in enumerate(vocab))
 inputs_train, queries_train, answers_train = vectorize_stories(train_stories,
                                                                word_idx,
                                                                story_maxlen,
                                                                query_maxlen)
+                                                               
+print(inputs_train[0])
+print(queries_train[0])
+print(answers_train[0])
+print(vocab)
+
 inputs_test, queries_test, answers_test = vectorize_stories(test_stories,
                                                             word_idx,
                                                             story_maxlen,
@@ -161,69 +171,142 @@ print('answers_test shape:', answers_test.shape)
 print('-')
 print('Compiling...')
 
-# placeholders
-input_sequence = Input((story_maxlen,))
-question = Input((query_maxlen,))
+#print('Exiting...')
+#exit()
+train = False
 
-# encoders
-# embed the input sequence into a sequence of vectors
-input_encoder_m = Sequential()
-input_encoder_m.add(Embedding(input_dim=vocab_size,
-                              output_dim=64))
-input_encoder_m.add(Dropout(0.3))
-# output: (samples, story_maxlen, embedding_dim)
+if train:
+  # placeholders
+  input_sequence = Input((story_maxlen,))
+  question = Input((query_maxlen,))
 
-# embed the input into a sequence of vectors of size query_maxlen
-input_encoder_c = Sequential()
-input_encoder_c.add(Embedding(input_dim=vocab_size,
-                              output_dim=query_maxlen))
-input_encoder_c.add(Dropout(0.3))
-# output: (samples, story_maxlen, query_maxlen)
+  # encoders
+  # embed the input sequence into a sequence of vectors
+  input_encoder_m = Sequential()
+  input_encoder_m.add(Embedding(input_dim=vocab_size, output_dim=64))
+  input_encoder_m.add(Dropout(0.3))
+  # output: (samples, story_maxlen, embedding_dim)
 
-# embed the question into a sequence of vectors
-question_encoder = Sequential()
-question_encoder.add(Embedding(input_dim=vocab_size,
-                               output_dim=64,
-                               input_length=query_maxlen))
-question_encoder.add(Dropout(0.3))
-# output: (samples, query_maxlen, embedding_dim)
+  # embed the input into a sequence of vectors of size query_maxlen
+  input_encoder_c = Sequential()
+  input_encoder_c.add(Embedding(input_dim=vocab_size, output_dim=query_maxlen))
+  input_encoder_c.add(Dropout(0.3))
+  # output: (samples, story_maxlen, query_maxlen)
 
-# encode input sequence and questions (which are indices)
-# to sequences of dense vectors
-input_encoded_m = input_encoder_m(input_sequence)
-input_encoded_c = input_encoder_c(input_sequence)
-question_encoded = question_encoder(question)
+  # embed the question into a sequence of vectors
+  question_encoder = Sequential()
+  question_encoder.add(Embedding(input_dim=vocab_size, output_dim=64, input_length=query_maxlen))
+  question_encoder.add(Dropout(0.3))
+  # output: (samples, query_maxlen, embedding_dim)
 
-# compute a 'match' between the first input vector sequence
-# and the question vector sequence
-# shape: `(samples, story_maxlen, query_maxlen)`
-match = dot([input_encoded_m, question_encoded], axes=(2, 2))
-match = Activation('softmax')(match)
+  # encode input sequence and questions (which are indices)
+  # to sequences of dense vectors
+  input_encoded_m = input_encoder_m(input_sequence)
+  input_encoded_c = input_encoder_c(input_sequence)
+  question_encoded = question_encoder(question)
 
-# add the match matrix with the second input vector sequence
-response = add([match, input_encoded_c])  # (samples, story_maxlen, query_maxlen)
-response = Permute((2, 1))(response)  # (samples, query_maxlen, story_maxlen)
+  # compute a 'match' between the first input vector sequence
+  # and the question vector sequence
+  # shape: `(samples, story_maxlen, query_maxlen)`
+  match = dot([input_encoded_m, question_encoded], axes=(2, 2))
+  match = Activation('softmax')(match)
 
-# concatenate the match matrix with the question vector sequence
-answer = concatenate([response, question_encoded])
+  # add the match matrix with the second input vector sequence
+  response = add([match, input_encoded_c])  # (samples, story_maxlen, query_maxlen)
+  response = Permute((2, 1))(response)  # (samples, query_maxlen, story_maxlen)
 
-# the original paper uses a matrix multiplication for this reduction step.
-# we choose to use a RNN instead.
-answer = LSTM(32)(answer)  # (samples, 32)
+  # concatenate the match matrix with the question vector sequence
+  answer = concatenate([response, question_encoded])
 
-# one regularization layer -- more would probably be needed.
-answer = Dropout(0.3)(answer)
-answer = Dense(vocab_size)(answer)  # (samples, vocab_size)
-# we output a probability distribution over the vocabulary
-answer = Activation('softmax')(answer)
+  # the original paper uses a matrix multiplication for this reduction step.
+  # we choose to use a RNN instead.
+  answer = LSTM(32)(answer)  # (samples, 32)
 
-# build the final model
-model = Model([input_sequence, question], answer)
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
-              metrics=['accuracy'])
+  # one regularization layer -- more would probably be needed.
+  answer = Dropout(0.3)(answer)
+  answer = Dense(vocab_size)(answer)  # (samples, vocab_size)
+  # we output a probability distribution over the vocabulary
+  answer = Activation('softmax')(answer)
 
-# train
-model.fit([inputs_train, queries_train], answers_train,
-          batch_size=32,
-          epochs=120,
-          validation_data=([inputs_test, queries_test], answers_test))
+  # build the final model
+  model = Model([input_sequence, question], answer)
+  model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+  # train
+  model.fit([inputs_train, queries_train], answers_train, batch_size=32, epochs=120,
+            validation_data=([inputs_test, queries_test], answers_test))
+
+
+  #write out the vocab as json file
+  with open('vocab.json', 'w') as outfile:
+    json.dump(vocab, outfile)
+    
+  #yaml_string = model.to_yaml()
+  #with open("model.yaml", "w") as text_file:
+  #    text_file.write(yaml_string)
+
+  json_string = model.to_json()
+  with open("model.json", "w") as text_file:
+    text_file.write(json_string)
+
+  model.save_weights('weights.hdf5')  
+else:
+  print("****************************")
+  print("******* Not training *******")
+  print("****************************")
+  
+  #print("YAML Model:")
+  #with open ("model.yaml", "r") as myfile:
+  #  yaml_string=myfile.read()
+  #print(yaml_string)
+
+
+  print("Importing JSON vocab:")
+  with open('vocab.json') as data_file:    
+      vocab = json.load(data_file)
+  word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
+  idx_word = dict((i + 1, c) for i, c in enumerate(vocab))
+  #print(vocab)
+  #print(word_idx)
+  #print(idx_word)
+
+  print("Importing JSON Model:")
+  with open ("model.json", "r") as myfile:
+    json_string=myfile.read()
+  #print(json_string)
+  model = model_from_json(json_string)
+
+  print("Importing Model Weights:")
+  model.load_weights('weights.hdf5')
+
+
+  print("Prediction:")
+  #9995 (['John', 'moved', 'to', 'the', 'bathroom', '.', 'Daniel', 'went', 'to', 'the', 'kitchen', '.'], ['Where', 'is', 'John', '?'], 'bathroom')
+  #9996 (['John', 'moved', 'to', 'the', 'bathroom', '.', 'Daniel', 'went', 'to', 'the', 'kitchen', '.', 'Sandra', 'travelled', 'to', 'the', 'kitchen', '.', 'Mary', 'travelled', 'to', 'the', 'hallway', '.'], ['Where', 'is', 'Daniel', '?'], 'kitchen')
+  #9997 (['John', 'moved', 'to', 'the', 'bathroom', '.', 'Daniel', 'went', 'to', 'the', 'kitchen', '.', 'Sandra', 'travelled', 'to', 'the', 'kitchen', '.', 'Mary', 'travelled', 'to', 'the', 'hallway', '.', 'Sandra', 'went', 'back', 'to', 'the', 'bathroom', '.', 'John', 'went', 'back', 'to', 'the', 'kitchen', '.'], ['Where', 'is', 'Mary', '?'], 'hallway')
+  #9998 (['John', 'moved', 'to', 'the', 'bathroom', '.', 'Daniel', 'went', 'to', 'the', 'kitchen', '.', 'Sandra', 'travelled', 'to', 'the', 'kitchen', '.', 'Mary', 'travelled', 'to', 'the', 'hallway', '.', 'Sandra', 'went', 'back', 'to', 'the', 'bathroom', '.', 'John', 'went', 'back', 'to', 'the', 'kitchen', '.', 'Daniel', 'went', 'back', 'to', 'the', 'office', '.', 'Daniel', 'journeyed', 'to', 'the', 'bathroom', '.'], ['Where', 'is', 'John', '?'], 'kitchen')
+  #9999 (['John', 'moved', 'to', 'the', 'bathroom', '.', 'Daniel', 'went', 'to', 'the', 'kitchen', '.', 'Sandra', 'travelled', 'to', 'the', 'kitchen', '.', 'Mary', 'travelled', 'to', 'the', 'hallway', '.', 'Sandra', 'went', 'back', 'to', 'the', 'bathroom', '.', 'John', 'went', 'back', 'to', 'the', 'kitchen', '.', 'Daniel', 'went', 'back', 'to', 'the', 'office', '.', 'Daniel', 'journeyed', 'to', 'the', 'bathroom', '.', 'John', 'went', 'back', 'to', 'the', 'office', '.', 'Mary', 'travelled', 'to', 'the', 'bedroom', '.'], ['Where', 'is', 'John', '?'], 'office')
+  #question = np.array([['John', 'moved', 'to', 'the', 'bathroom', '.', 'Daniel', 'went', 'to', 'the', 'kitchen', '.'], ['Where', 'is', 'John', '?']])
+  question = [inputs_test, queries_test]
+  #print(np.shape(question))
+  print(np.shape(inputs_test))
+  print(np.shape(queries_test))
+  print(question)
+  answer = model.predict(question)
+  print(answer[0])
+  print(test_stories[0])
+
+#####################################################################################################
+# chatbot
+#####################################################################################################
+# Take a look at how the training data is stored (incase I want to add some additional data)
+# Figure out how to save Keras Model (and weights) so that I don't have to train it over and over.
+# First two steps were easy enough, decided to try to do some prediction.
+# Keras prediction function looks easy enough, but I probably need to ask the question the right way
+# -> This is causing me to read code to try an understand how to ask the question
+# -> If I can get and answer I am concerned that the answer I get may be some vectors
+# --> I'm assuming that if the answer is in vector form that I will need to decode back to english
+# Stored vectors integers from the vocab lookup table, this table will need to be saved and loaded.
+          
+
+
